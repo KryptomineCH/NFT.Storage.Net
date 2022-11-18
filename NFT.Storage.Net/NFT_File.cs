a50309ab246b23d7ec8ee566925a437874f4124c
Julian B <bechti44@googlemail.com>
2022-11-18 09:58:08 +0100

    Add project files.

---
+++ NFT.Storage.Net/NFT_File.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFT.Storage.Net
+{
+    public class NFT_File
+    {
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public string Sha256Sum { get; set; }
+    }
+}
