7ecfa46d7cf700f871f45f72c0d8764c6c032862
Julian B <bechti44@googlemail.com>
2022-11-22 13:06:05 +0100

    added api key

---
+++ NFT.Storage.Net.UnitTests/GlobalVar.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.IO;
+
+namespace NFT.Storage.Net.UnitTests
+{
+    internal class GlobalVar
+    {
+        public GlobalVar()
+        {
+            TestApiKey = File.ReadAllText("ApiKey.txt");
+        }
+        /// <summary>
+        /// note tó update the key in ApiKey.txt
+        /// </summary>
+        internal static string TestApiKey { get; set; }
+    }
+}
---
+++ NFT.Storage.Net.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace NFT.Storage.Net.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
