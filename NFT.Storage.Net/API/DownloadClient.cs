4be4598fa9c302bb9e6a8ff9a5b834ec4eb1cf04
KryptomineCH <julian.bechtold@kryptomine.ch>
2023-02-01 22:51:49 +0100

    added update progress bar and fixed ui freeze on update

--- NFT.Storage.Net/API/DownloadClient.cs
+++ NFT.Storage.Net/API/DownloadClient.cs
@@ -21,23 +21,23 @@ namespace NFT.Storage.Net.API
             _Client.DefaultRequestHeaders.ConnectionClose = false;
         }
         private static HttpClient _Client;
-        public static byte[] DownloadSync(Uri url)
+        public static byte[] DownloadSync(Uri url, IProgress<double> progress = null)
         {
-            Task<byte[]> data = Task.Run(() => DownloadClient.DownloadAsync(url));
+            Task<byte[]> data = Task.Run(() => DownloadClient.DownloadAsync(url, progress));
             data.Wait();
             return data.Result;
         }
-        public static byte[] DownloadSync(string url)
+        public static byte[] DownloadSync(string url, IProgress<double> progress = null)
         {
-            Task<byte[]> data = Task.Run(() => DownloadClient.DownloadAsync(url));
+            Task<byte[]> data = Task.Run(() => DownloadClient.DownloadAsync(url, progress));
             data.Wait();
             return data.Result;
         }
-        public static async Task<byte[]> DownloadAsync(Uri url)
+        public static async Task<byte[]> DownloadAsync(Uri url, IProgress<double> progress = null)
         {
-            return await DownloadAsync(url.ToString());
+            return await DownloadAsync(url.ToString(),progress);
         }
-        public static async Task<byte[]> DownloadAsync(string url)
+        public static async Task<byte[]> DownloadAsync(string url, IProgress<double> progress = null)
         {
             for (int retries = 0; retries < 3; retries++)
             {
@@ -58,9 +58,29 @@ namespace NFT.Storage.Net.API
                     await Task.Delay(50);
                     continue;
                 }
-                System.Net.Http.HttpContent content = response.Content;
-                byte[] file = await content.ReadAsByteArrayAsync();
-                return file;
+                long contentLength = response.Content.Headers.ContentLength.GetValueOrDefault();
+                if (contentLength <= 0 || progress == null)
+                {
+                    // If content length is unknown, read the entire response as a stream.
+                    byte[] file = await response.Content.ReadAsByteArrayAsync();
+                    return file;
+                }
+
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                {
+                    int bufferSize = 1024 * 4;
+                    var buffer = new byte[bufferSize];
+                    int bytesRead;
+                    long totalBytesRead = 0;
+
+                    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        totalBytesRead += bytesRead;
+                        progress?.Report((double)totalBytesRead / contentLength);
+                    }
+
+                    return buffer.Take((int)totalBytesRead).ToArray();
+                }
             }
             throw new HttpRequestException("File Download failed!");
         }
